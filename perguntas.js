670f938ebab12d10ca199327f61290dd36b2f53c
angelo.zampier <angelo.zampier@escola.pr.gov.br>
2025-08-14 08:50:21 -0300

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Belchior Marques Goulart '
+)
+
+criaCartao(
+    'Química',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118.'
+)
+
+criaCartao(
+    'fisica',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    ' 8 minutos. 150 000 000 dividido por 300 000 é igual a 500 segundos, ou seja, 8,33 minutos.
+
+'
+)
+    criaCartao(
+    'arte',
+    'Qual desses filmes foi baseado na obra de Shakespeare?',
+    'Muito Barulho por Nada (2012).'
+)
